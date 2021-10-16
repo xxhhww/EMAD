7f0521d29670597d352e2f0c256c963d1e090a7b
xxhhww <2908903586@qq.com>
2021-10-16 15:20:33 +0800

    Add and test C++ mathematics library(GLM)

--- EMAD/Renderer.cpp
+++ EMAD/Renderer.cpp
@@ -4,6 +4,10 @@
 
 #include "stb_image.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
 
 App::App(int width, int height, const std::string& name) noexcept
@@ -14,7 +18,14 @@ App::~App()
 }
 
 int App::run()
-{ 
+{
+    //test glm
+    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+    vec = trans * vec;
+    std::cout << vec.x << vec.y << vec.z << std::endl;
+
     //创建着色器程序
     Program testProgram("Shader/test_vs.vert", "Shader/test_fs.frag");
 
